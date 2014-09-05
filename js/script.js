a74191434a400baadc12fd2a0cf26fac5364552f
Christopher Thompson <cyberstorm305@gmail.com>
2014-09-05 15:55:34 +0100

    Search Query Example

--- js/script.js
+++ js/script.js
@@ -10,4 +10,27 @@ jQuery(document).ready(function(){
         $('#cart').modal('hide');
         $('#export').modal('show');
     });
+    $('#searchBtn').click(function(){
+        var root = 'http://homercreative.github.io/prototype/search-results.html';
+        var get = '?search=' + $('#searchText').text();
+        window.location.href = root + get;
+    });
+
+
+    var searchString = getUrlParameter('search');
+    $('#searchText').text(searchString);
+
+    function getUrlParameter(sParam)
+    {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    }
 });
