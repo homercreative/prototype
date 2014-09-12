eef69187ec58dc77cfdecbb6c7245e6e51f419e1
Christopher Thompson <cyberstorm305@gmail.com>
2014-09-12 20:11:41 +0100

    Start to new boilerplate

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = ""
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+sass_options = {:debug_info => true}
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+line_comments = false
