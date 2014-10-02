fa90be1a978ab5d2713068b97ed22e0bb434df4d
Christopher Thompson <cyberstorm305@gmail.com>
2014-10-02 14:58:11 +0100

    Live commit for the javascript

--- js/script.js
+++ js/script.js
@@ -2423,17 +2423,31 @@ function loadFontKit($fontKit) {
 
 $('document').ready(function(){
 
+    // Script constants
+
+    var number = 1;
+    var resultBlock = $('.result-block');
+
+    // Modal close animation
+
     var closeModal = function($element){
         var id = $element.parent().parent().parent().parent().attr('id');
         $("#" + id).modal('hide');
         $element.parent().parent().parent().removeClass('flipOutX');
     };
-    $('.result-block').click(function(){
+
+    // Shows the modal box for each of the results
+
+    resultBlock.click(function(){
         var id = $(this).attr('id');
         var value = id.replace('result', 'extra');
         var element = $('#' + value);
         element.modal('show');
     });
+
+    // Nav link item click function to change the colours per section for previewing the colours
+    // this will need to be replaced to make them tabs in the live version
+
     $('.navlink').click(function(){
        $('.navlink').each(function(){
            $(this).removeClass('active')
@@ -2443,23 +2457,22 @@ $('document').ready(function(){
         var text = $(this).text();
         $('body').attr('id',text.toLowerCase().replace(/ /g, '-'));
     });
+
+    // Export modal button clicks shows the modal, hides the cart modal
+
     $('#exportButton').click(function(){
         $('#cart').modal('hide');
         $('#export').modal('show');
     });
-    $('#search').submit(function(e){
-        e.preventDefault();
-        var url = 'http://homercreative.github.io/prototype/search-results.html';
-        var get = '?search=' + $('#searchText').val();
-        console.log(url + get + '');
-        window.location.href = (url + get + '');
-    });
 
-    var number = 1;
-    $('.result-block').each(function(){
+    // Number for each item this makes the numbering dynamic and not set statically
+    resultBlock.each(function(){
         $(this).find('.number').html(number);
         number++;
     });
+
+    // Close button closes the modal with the animation
+
     $('.close-button').click(function(e){
         e.preventDefault();
         var element = $(this);
@@ -2467,33 +2480,13 @@ $('document').ready(function(){
         setTimeout(closeModal, 2200, element);
     });
 
-        $(".owl-carousel").owlCarousel({
-            items: 1,
-            loop: false,
-            nav: false,
-            dots: true,
-            dotsData : true
-        });
+    // Carousel load to handle the swiping nature of the results page
 
-
-    var searchString = decodeURI(getUrlParameter('search'));
-
-    if(searchString != 'undefined'){
-        $('#searchText').val(searchString);
-    }
-
-
-    function getUrlParameter(sParam)
-    {
-        var sPageURL = window.location.search.substring(1);
-        var sURLVariables = sPageURL.split('&');
-        for (var i = 0; i < sURLVariables.length; i++)
-        {
-            var sParameterName = sURLVariables[i].split('=');
-            if (sParameterName[0] == sParam)
-            {
-                return sParameterName[1];
-            }
-        }
-    }
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: false,
+        nav: false,
+        dots: true,
+        dotsData : true
+    });
 });
